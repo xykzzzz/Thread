ccf7ce282a82675cb551c917d1b2344b0febbf82
xykzzzz <1105054453@qq.com>
2021-03-17 20:56:08 +0800

    死锁演示

---
+++ 死锁代码/Test.java
@@ -0,0 +1,50 @@
+package process;
+
+class Pen {
+    private String pen = "笔" ;
+    public String getPen() {
+        return pen;
+    }
+}
+class Book {
+    private String book = "本" ;
+    public String getBook() {
+        return book;
+    }
+}
+public class Test {
+    private static Pen pen = new Pen() ;
+    private static Book book = new Book() ;
+    public static void main(String[] args) {
+        new Test().deadLock();
+    }
+    public void deadLock() {
+        Thread thread1 = new Thread(new Runnable() { // 笔线程
+            @Override
+            public void run() {
+                synchronized (pen) {
+                    System.out.println(Thread.currentThread()+" :我有笔，我就不给你");
+                    synchronized (book) {
+                        System.out.println(Thread.currentThread()+" :把你的本给我！");
+                    }
+                }
+            }
+        },"Pen") ;
+
+        Thread thread2 = new Thread(new Runnable() { // 本子线程
+            @Override
+            public void run() {
+                synchronized (book) {
+                    System.out.println(Thread.currentThread()+" :我有本子，我就不给你！");
+                    synchronized (pen) {
+                        System.out.println(Thread.currentThread()+" :把你的笔给我！");
+                    }
+                }
+
+            }
+        },"Book") ;
+        thread1.start();
+        thread2.start();
+    }
+}
+
